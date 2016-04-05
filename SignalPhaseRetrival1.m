bb24329857312fee6f0d6f6f4f0adf313b1081a3
kenkangxgwe <kenkangxgwe@gmail.com>
2016-04-05 23:52:21 +0800

    using a block while optimizing the subtract. Meanwhile, add a 3Dslice plot at the end

--- SignalPhaseRetrival1.m
+++ SignalPhaseRetrival1.m
@@ -12,16 +12,23 @@ else
     error('ReferencePhase.mat does not exist');
 end
 
-fun1 = @(x) rms(abs( (holI (holI <= 1200) - x^2 * refI (holI <= 1200) ) ) );
+% Set a block at [300:480,250:750]
+block = ones(1040,1392);
+block(300:480, 250:750) = 0;
+
+fun1 = @(x) var( (holI (block == 1) - x^2 * refI (block == 1) ) );
 [coef] = fminunc(fun1,1);
 coRefI = coef^2;
-holDiff = abs(holI - coRefI * refI);
+holDiff = (holI - coRefI * refI);
+refDiff = refI - refBg;
+holDiff = holDiff .* (refDiff >= 0.01);
+refDiff = max(refDiff, 0.01);
 
-fun2 = @(x) rms(rms(holDiff - x(1)^2 * holBg + x(2)^2 * refBg) );
-[coef] = fminunc(fun2,[1,1]);
-coHolBg = coef(1)^2;
-coRefBg = coef(2)^2;
-holDiff = holDiff - coHolBg * holBg + coRefBg * refBg;
+% fun2 = @(x) rms(rms(holDiff - x(1)^2 * holBg + x(2)^2 * refBg) );
+% [coef] = fminunc(fun2,[1,1]);
+% coHolBg = coef(1)^2;
+% coRefBg = coef(2)^2;
+% holDiff = holDiff - coHolBg * holBg + coRefBg * refBg;
 
 % fun = @(x) rms(rms( (holDiff - x(1)^2 * holBg + x(2)^2 * refBg) .* (holI <= 1020) ) );
 % [coef] = fminunc(fun2,[1,1]);
@@ -31,15 +38,7 @@ holDiff = holDiff - coHolBg * holBg + coRefBg * refBg;
 % holDiff = abs(holDiff);
 
 holDiff = double(imresize(holDiff, resize, 'nearest') );
-refE = double(imresize(sqrt(refI), resize, 'nearest') ); % E field = sqrt(Amplitude)
-refE = max(refE, 1);
-% Denoising, threshold is max(srcimg(:) ) / 300
-% refI = refI - max(refI(:) ) / 300;
-% refI = refI .* (refI > 0); 
-% holI = holI .* (refI > 0) + (refI + max(refI(:) ) / 300) .* (refI <= 0);
-% refI = refI + max(refI(:) ) / 300;
-% 
-% gpuRefI  = gpuArray(refI);
+refE = double(imresize(sqrt(refDiff), resize, 'nearest') ); % E = sqrt(I)
 
 holE = holDiff./ (refE .* exp(- 1i * refPhi) );
 
@@ -64,12 +63,19 @@ nxDisp=2000:4000;
 nyDisp=2000:4000;
 gpuHolE = gpuArray(holE);
 
-% figure
-% for sigZ = (- 0.0505 * 25400) : -100 : (- 0.6050 * 25400)
-sigZ = -12500;
-sigE = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (sigZ - refZ) ) .* fft2(gpuHolE , eySize, exSize) );
-figure; imagesc(abs(sigE(nyDisp,nxDisp) ) ); colorbar; title(['sigZ = ' num2str(sigZ)]);
-sigE = sigE .* (abs(sigE) < 20);
-sigE2 = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (refZ - sigZ) ) .* fft2(sigE , eySize, exSize) );
-figure; imagesc(abs(sigE2) ); colorbar; title(['sigZ = ' num2str(refZ)]);
- end
+x = 1:6:6240;
+y = 1:6:8352;
+z = 1;
+figure;
+for sigZ = (- 0.0505 * 25400) : -100 : (- 0.6050 * 25400)
+    % sigZ = -12500;
+    sigE = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (sigZ - refZ) ) .* fft2(gpuHolE , eySize, exSize) );
+%     imagesc(abs(sigE(nyDisp,nxDisp) ) ); colorbar; title(['sigZ = ' num2str(sigZ)]);
+%     drawnow;
+    z = z + 1
+    spaceMat(:, :, z) = abs(sigE(x,y) );
+end
+
+[x,y,z] = meshgrid(1:1392,1:1040,1:142);
+zslice = 1:142; 
+slice(x, y, z, spaceMat, [], [], zslice);
