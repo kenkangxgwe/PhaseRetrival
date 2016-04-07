1c99507702f818549ad65ccba22d6589edb703b5
kenkangxgwe <kenkangxgwe@gmail.com>
2016-04-07 22:30:01 +0800

    a new way to determine the steps that wish to go through.

--- phaseRetrivalCaller.m
+++ phaseRetrivalCaller.m
@@ -5,7 +5,7 @@
 
 wavelength = 0.7667;
 pixelsize = 6.5;
-step = 3;
+step = 2;
 path = 'G:\新建文件夹\Experiment\20Mar2016\';
 
 % bg1 = openxlsFigures(path, 'BK', 1, 10, 'up');
@@ -15,7 +15,7 @@ refZ = - 0.0505 * 25400; % Distance in mm
 %% RefPhaseRetrival1
 % A iterative algorithm to get the phase infomation of image a1
 
-if(step < 2)
+if(bitand(step, 1) )
     %From top to bottom, the distance decreases and the light spot shrinks
     a1 = openxlsFigures(path, 'waveB', 1, 100, 'up');
     bg1 = openxlsFigures(path, 'waveBbk', 1, 100, 'up');
@@ -44,7 +44,7 @@ end
 % Using angular spectrum method to get the phase infomation of reference
 % image via image a1
 
-if(step < 3)
+if(bitand(step, 2) )
     refimg = openxlsFigures(path, 'waveA', 1, 100, 'up');
     refbg = openxlsFigures(path, 'waveAbk', 1, 100, 'up');
     refimg = max(refimg - refbg, 0);
@@ -53,7 +53,7 @@ if(step < 3)
     a1 = max(a1 - bg1, 0);
     z1 = - 0.2500 * 25400; 
 
-    [refPhi] = RefPhaseRetrival2(a1, z1, refZ, pixelsize, wavelength, path);
+    [refPhi] = RefPhaseRetrival2(a1, z1, refimg, refZ, pixelsize, wavelength, path);
     save([path 'Result\ReferencePhase.mat'],'refPhi');
 end
 
@@ -61,7 +61,7 @@ end
 % Get the signal holograph via reference intensity, phase and signal
 % intensity
 
-if(step < 4)
+if(bitand(step, 4) )
 %     atomCut = openxlsFigures(path, 'p10n10acut', 8, 15, 'up');
 %     atomCutRef = openxlsFigures(path, 'p10n10acut', 8, 15, 'down');
     atomCut = openxlsFigures(path, 'fscancut', 1, 1, 'up');
