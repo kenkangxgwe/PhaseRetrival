7587d11bf227d4ab9c09a1fd240ecbc2b38f7eb6
kenkangxgwe <kenkangxgwe@gmail.com>
2016-04-09 19:38:50 +0800

    change path for step 100b

--- phaseRetrivalCaller.m
+++ phaseRetrivalCaller.m
@@ -5,7 +5,7 @@
 
 wavelength = 0.7667;
 pixelsize = 6.5;
-step = 3;
+step = 4;
 path = 'G:\新建文件夹\Experiment\7Apr2016\';
 
 refZ = - 0.0505 * 25400; % Distance in mm
@@ -15,23 +15,23 @@ refZ = - 0.0505 * 25400; % Distance in mm
 
 if(bitand(step, 1) )
     %From top to bottom, the distance decreases and the light spot shrinks
-    a1 = openxlsFigures(path 'WavefrontMeasurement', 'waveB', 1, 100, 'up');
-    bg1 = openxlsFigures(path 'WavefrontMeasurement', 'waveBbk', 1, 100, 'up');
+    a1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveB', 1, 100, 'up');
+    bg1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveBbk', 1, 100, 'up');
     a1 = max(a1 - bg1, 0);
     z1 = - 0.2721 * 25400; 
 
-    a2 = openxlsFigures(path 'WavefrontMeasurement', 'waveC', 1, 100, 'up');
-    bg2 = openxlsFigures(path 'WavefrontMeasurement', 'waveCbk', 1, 100, 'up');
+    a2 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveC', 1, 100, 'up');
+    bg2 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveCbk', 1, 100, 'up');
     a2 = max(a2 - bg2, 0);
     z2 = - 0.3500 * 25400;
     
-    a3 = openxlsFigures(path 'WavefrontMeasurement', 'waveD', 1, 100, 'up');
-    bg3 = openxlsFigures(path 'WavefrontMeasurement', 'waveDbk', 1, 100, 'up');
+    a3 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveD', 1, 100, 'up');
+    bg3 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveDbk', 1, 100, 'up');
     a3 = max(a3 - bg3, 0);
     z3 = - 0.5000 * 25400;
     
-    a4 = openxlsFigures(path 'WavefrontMeasurement', 'waveE', 1, 100, 'up');
-    bg4 = openxlsFigures(path 'WavefrontMeasurement', 'waveEbk', 1, 100, 'up');
+    a4 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveE', 1, 100, 'up');
+    bg4 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveEbk', 1, 100, 'up');
     a4 = max(a4 - bg4, 0);
     z4 = - 0.6080 * 25400;
 
@@ -43,11 +43,11 @@ end
 % image via image a1
 
 if(bitand(step, 2) )
-    refimg = openxlsFigures(path 'WavefrontMeasurement', 'waveA', 1, 100, 'up');
-    refbg = openxlsFigures(path 'WavefrontMeasurement', 'waveAbk', 1, 100, 'up');
+    refimg = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveA', 1, 100, 'up');
+    refbg = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveAbk', 1, 100, 'up');
     refimg = max(refimg - refbg, 0);
-    a1 = openxlsFigures(path 'WavefrontMeasurement', 'waveB', 1, 100, 'up');
-    bg1 = openxlsFigures(path 'WavefrontMeasurement', 'waveBbk', 1, 100, 'up');
+    a1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveB', 1, 100, 'up');
+    bg1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveBbk', 1, 100, 'up');
     a1 = max(a1 - bg1, 0);
     z1 = - 0.2500 * 25400; 
 
@@ -60,19 +60,10 @@ end
 % intensity
 
 if(bitand(step, 4) )
-%     atomCut = openxlsFigures(path, 'p10n10acut', 8, 15, 'up');
-%     atomCutRef = openxlsFigures(path, 'p10n10acut', 8, 15, 'down');
-    atomCut = openxlsFigures(path, 'fscancut', 1, 1, 'up');
-    atomCutRef = openxlsFigures(path, 'fscancut', 1, 1, 'down');
-    atomCutBk1 = openxlsFigures(path, 'fscannolabk', 1, 6, 'up');
-    atomCutBk2 = openxlsFigures(path, 'fscannolabk', 1, 6, 'down');
-%     atomNoCut = openxlsFigures(path, 'p10n10acut', 14, 14, 'up');
-%     at    omNoCutRef = openxlsFigures(path, 'p10n10acut', 14, 14, 'down');
-
-%     atomNoCut = max(atomNoCut - bg1, 10);
-%     atomNoCutRef = max(atomNoCutRef - bg1, 10);
-%     atomCut = max(atomCut - bg1, 10);
-%     atomCutRef = max(atomCutRef - bg1, 10);
+    atomCut = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'Dipoletrap_MOT1d0V_Exp1d0ms', 200, 200, 'up');
+    atomCutRef = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'Dipoletrap_MOT1d0V_Exp1d0ms', 200, 200, 'down');
+    atomCutBk1 = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'BK', 2, 10, 'up');
+    atomCutBk2 = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'BK', 2, 10, 'down');
 
     SignalPhaseRetrival1(atomCut, atomCutRef, refZ, atomCutBk1, atomCutBk2, pixelsize, wavelength, path);
 end
