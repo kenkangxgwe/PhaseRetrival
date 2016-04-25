eff00798c948b5ef457d37ff3edc2724ef170bb5
kenkangxgwe <kenkangxgwe@gmail.com>
2016-04-25 17:28:55 +0800

    overloadable when open one file

--- openxlsFigures.m
+++ openxlsFigures.m
@@ -1,24 +1,33 @@
-function ImageArray = openxlsFigures(FilePath, FileName, head, tail, pos)
+function ImageArray = openxlsFigures(FilePath, FileName, pos, head, tail)
 
 impPath = [FilePath];
 expPath = [FilePath 'Merge\'];
 
-loadsave = 1;
-if(loadsave && exist([expPath FileName pos '.mat'], 'file') )
+loadexist = 1;
+if(loadexist && exist([expPath FileName pos '.mat'], 'file') )
     load([expPath FileName pos '.mat'], 'ImageArray');
     return;
 end
 
 ImageArray = 0;
 
+if(nargin == 3)
+    head = 0;
+    tail =head;
+end
+
 for ii = head : tail
-    openFile = [impPath FileName num2str(ii) '.xls'];
+    if(nargin == 3)
+        openFile = [impPath FileName '.xls'];
+    else
+        openFile = [impPath FileName num2str(ii) '.xls'];
+    end
     RawImage = fopen(openFile, 'r');
-    ImageArray = ImageArray + fread(RawImage, [1392 2080],'uint16', 'ieee-be')';
+    ImageArray = ImageArray + fread(RawImage, [1392 2080], 'uint16', 'ieee-be')';
     %     ImageArray = ImageArray + (fread(RawImage, [1392 2080], 'uint16', 'ieee-be')') .^ (-2);
     fclose(RawImage);
 end
-
+    
 if(strcmp(pos, 'up') )
     ImageArray = ImageArray(1 : 1040, :);
 else if(strcmp(pos, 'down') )
