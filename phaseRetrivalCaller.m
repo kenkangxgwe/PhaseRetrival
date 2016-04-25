f7a1508dd3072aa31c97b1bf83974cd64abae952
kenkangxgwe <kenkangxgwe@gmail.com>
2016-04-25 17:31:16 +0800

    change the order of argins according to the new openxlsFigures

--- phaseRetrivalCaller.m
+++ phaseRetrivalCaller.m
@@ -15,23 +15,23 @@ refZ = - 0.0505 * 25400; % Distance in mm
 
 if(bitand(step, 1) )
     %From top to bottom, the distance decreases and the light spot shrinks
-    a1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveB', 1, 100, 'up');
-    bg1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveBbk', 1, 100, 'up');
+    a1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveB', 'up', 1, 100);
+    bg1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveBbk', 'up', 1, 100);
     a1 = max(a1 - bg1, 0);
     z1 = - 0.2721 * 25400; 
 
-    a2 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveC', 1, 100, 'up');
-    bg2 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveCbk', 1, 100, 'up');
+    a2 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveC', 'up', 1, 100);
+    bg2 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveCbk', 'up', 1, 100);
     a2 = max(a2 - bg2, 0);
     z2 = - 0.3500 * 25400;
     
-    a3 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveD', 1, 100, 'up');
-    bg3 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveDbk', 1, 100, 'up');
+    a3 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveD', 'up', 1, 100);
+    bg3 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveDbk', 'up', 1, 100);
     a3 = max(a3 - bg3, 0);
     z3 = - 0.5000 * 25400;
     
-    a4 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveE', 1, 100, 'up');
-    bg4 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveEbk', 1, 100, 'up');
+    a4 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveE', 'up', 1, 100);
+    bg4 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveEbk', 'up', 1, 100);
     a4 = max(a4 - bg4, 0);
     z4 = - 0.6080 * 25400;
 
@@ -43,11 +43,11 @@ end
 % image via image a1
 
 if(bitand(step, 2) )
-    refimg = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveA', 1, 100, 'up');
-    refbg = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveAbk', 1, 100, 'up');
+    refimg = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveA', 'up', 1, 100);
+    refbg = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveAbk', 'up', 1, 100);
     refimg = max(refimg - refbg, 0);
-    a1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveB', 1, 100, 'up');
-    bg1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveBbk', 1, 100, 'up');
+    a1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveB', 'up', 1, 100);
+    bg1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveBbk', 'up', 1, 100);
     a1 = max(a1 - bg1, 0);
     z1 = - 0.2500 * 25400; 
 
@@ -60,10 +60,10 @@ end
 % intensity
 
 if(bitand(step, 4) )
-    atomCut = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'Dipoletrap_MOT1d0V_Exp1d0ms', 200, 200, 'up');
-    atomCutRef = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'Dipoletrap_MOT1d0V_Exp1d0ms', 200, 200, 'down');
-    atomCutBk1 = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'BK', 2, 10, 'up');
-    atomCutBk2 = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'BK', 2, 10, 'down');
+    atomCut = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'Dipoletrap_MOT1d0V_Exp1d0ms202', 'up');
+    atomCutRef = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'Dipoletrap_MOT1d0V_Exp1d0ms202', 'down');
+    atomCutBk1 = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'BK', 'up', 2, 10);
+    atomCutBk2 = openxlsFigures(strcat(path,'Dipoletrap_MOT1d0V_Exp1d0ms\'), 'BK', 'down', 2, 10);
     [coRef, coHolbg, coRefbg] = HologramOptimization1(atomCut, atomCutRef, refZ, atomCutBk1, atomCutBk2, pixelsize, wavelength, path);
     SignalImageRetrival1(atomCut, coRef * atomCutRef, refZ, coHolbg * atomCutBk1, coRefbg * atomCutBk2, pixelsize, wavelength, path);
 end
