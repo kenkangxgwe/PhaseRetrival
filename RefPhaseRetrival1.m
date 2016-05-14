4d6b22a3e542028ce85b66c6cffa6c466838d9d5
kenkangxgwe <kenkangxgwe@gmail.com>
2016-05-14 22:08:38 +0800

    No Denoise. An optimization added.

--- RefPhaseRetrival1.m
+++ RefPhaseRetrival1.m
@@ -1,4 +1,6 @@
-function RefPhaseRetrival1(image0, image1, image2, image3, z0, z1, z2, z3, pixelsize, wavelength, FilePath)
+function RefPhaseRetrival1c(image0, image1, image2, image3, z0, z1, z2, z3, pixelsize, wavelength, FilePath)
+% In this version, the image is readjusted at each image plane along x-y
+% directions. the results seem unstable...
 % image0 focal
 % image1 small
 % image2 medium
@@ -8,22 +10,16 @@ function RefPhaseRetrival1(image0, image1, image2, image3, z0, z1, z2, z3, pixel
 
 gpuDevice(1);
 resize = 6;
-k = 2 * pi / wavelength;  %Wave Vector
-NAs = 0.4;
+k = 2 * pi / wavelength;  % Wave Vector
+NAs = 0.35;
+displayN=100; % Display the intermediate result every N iterations
+
 resPath = [FilePath '\Result\'];
 
 image0 = double(imresize(image0, resize, 'nearest') );
 image1 = double(imresize(image1, resize, 'nearest') );
 image2 = double(imresize(image2, resize, 'nearest') );
 image3 = double(imresize(image3, resize, 'nearest') );
-
-%Denoising, threshold = max(nimage(:) )/300
-image1 = image1 - max(image1(:) ) / 300;
-image1 = sqrt(image1 .* (image1 > 0) );
-image2 = image2 - max(image2(:) ) / 300;
-image2 = sqrt(image2 .* (image2 > 0) );
-image3 = image3 - max(image3(:) ) / 300;
-image3 = sqrt(image3 .* (image3 > 0) );
 
 mesh = pixelsize/resize;
 [ly, lx] = size(image0);
@@ -37,7 +33,7 @@ L = (z3 - z0);
 xx = gpuArray(xx);
 yy = gpuArray(yy);
 rxy2 = (xx - x0) .^ 2 + (yy - y0) .^ 2;
-iniPhi = k * (sqrt( (rxy2 + L^2) ) - L);  %initial phase
+iniPhi = k * (sqrt( (rxy2 + L^2) ) - L);  % initial phase
 clear image0
 clear xx
 clear yy
@@ -45,62 +41,78 @@ clear rxy2
 
 kmeshx = 2 * pi / (mesh * lx);
 kmeshy = 2 * pi / (mesh * ly);
-kx = ( (1 : lx) - lx / 2 - 1) * kmeshx;  %kx = kx-mean(kx);
-ky = ( (1 : ly) - ly / 2 - 1) * kmeshy;  %ky = ky-mean(ky);
+kx = ( (1 : lx) - lx / 2 - 1) * kmeshx;  % kx = kx-mean(kx);
+ky = ( (1 : ly) - ly / 2 - 1) * kmeshy;  % ky = ky-mean(ky);
+
 kx = fftshift(kx);
 ky = fftshift(ky);
 [kkx, kky] = meshgrid(kx, ky);
 kkx = gpuArray(kkx);
 kky = gpuArray(kky);
 kwindow = exp(- (kkx .^ 2 + kky .^ 2) / k^2 / NAs^2);
+kwindow2 = kwindow > (max(kwindow(:) ) / 1.01);
 kwindow = kwindow > (max(kwindow(:) ) / 2.71828);
+
+coef=[0 0 0 1];
 
 if(exist([resPath 'currentPhase.mat'], 'file') )
     load([resPath 'currentPhase.mat']);
 else
-    curPhi = iniPhi;  %current phase
+    curPhi = iniPhi;  % current phase
 end
 
-gpuImage1 = gpuArray(image1);
-gpuImage2 = gpuArray(image2);
-gpuImage3 = gpuArray(image3);
+gpuImage1=real(ifft2(kwindow2 .* fft2(gpuArray(image1))));
+gpuImage2=real(ifft2(kwindow2 .* fft2(gpuArray(image2))));
+gpuImage3=real(ifft2(kwindow2 .* fft2(gpuArray(image3))));
+clear kwindow2;
 
 if (~(exist('j', 'var') ) )
     j = 0;
 end
 inij = j;
 
+% Options for fiminc in the iteration
+options = optimoptions(@fminunc,'Display','iter','Algorithm','quasi-newton');
+options.TolX=1e-8;
+options.TolFun=1e-8;
+options.MaxFunEvals= 1000;
+
 while j < 100001
     j = j + 1
-    % cep stands for current estimated picture
-    cep = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (z2 - z3) ) .* fft2(gpuImage3 .* exp(1i * curPhi) ));
+%     cep stands for current estimated picture
+%     cep = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (z2 - z3) ) .* fft2(gpuImage3 .* exp(1i * curPhi) ) );
+%     curPhi = angle(cep);
+    
+    cep = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (z1 - z3) ) .* fft2(gpuImage3 .* exp(1i * curPhi) ) );
     curPhi = angle(cep);
     
-    cep = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (z1 - z2) ) .* fft2(gpuImage2 .* exp(1i * curPhi) ));
+    if (mod(j , displayN) ==0)
+        cep=fft2(gpuImage1 .* exp(1i * curPhi));
+        fun1 = @(x)gather(sum(sum( abs( (abs(ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (z3 - z1+x(1) )+x(2)*kkx.^2+x(3)*kky.^2 ).*cep) ) ).^2*x(4)-gpuImage3.^2) ) ) );
+        [coef] = fminunc(fun1, coef, options)
+        cep=ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (z3 - z1+coef(1) ) +coef(2)*kkx.^2+coef(3)*kky.^2).*cep);
+    else
+        cep = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (z3 - z1) ) .* fft2(gpuImage1 .* exp(1i * curPhi) ) );
+    end
     curPhi = angle(cep);
     
-    cep = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (z3 - z1) ) .* fft2(gpuImage1 .* exp(1i * curPhi) ));
-    curPhi = angle(cep);
-    
-    if (mod(j - inij, 100) == 1)
+    if (mod(j , displayN) == 0)
         subplot(2, 2, 1);
         imagesc(x, y, gpuImage3);  colorbar; title(['iteration = ' num2str(j) ',image3 to image 3']);
-        %     subplot(1,3,2);
-        %     imagesc(res32); caxis([0 1]);  colorbar; title(['iteration = ' num2str(j) ',image2 to image 3']);
+        
         subplot(2, 2, 2);
-        imagesc(x, y, (abs(cep) ) ); colorbar; title(['iteration = ' num2str(j) ',ep3 ']);
+        imagesc(x, y, coef(4)^.5 * (abs(cep) ) ); colorbar; title(['iteration = ' num2str(j) ',ep3 ']);
         
         subplot(2, 2, 3);
         imagesc(x, y, sin(curPhi - iniPhi) ); colorbar; title(['iteration = ' num2str(j) ',angle ']);
         
-        res13 = abs( (abs(cep) ) .^ 2 - (gpuImage3) .^ 2) ./ max( (gpuImage3(:) ) .^ 2);
-        res1m( ceil( (j - inij) / 100 ) ) = mean(mean(res13) );
+        res13 = abs( coef(4) * (abs(cep) ).^2  - (gpuImage3).^2) / max( (gpuImage3(:) .^2) );
+        res1m( ceil( (j - inij) / displayN ) ) = mean(mean(res13) );
         
         subplot(2,2,4);
-        plot(inij+1: 100 : j ,res1m(1, ceil( (j - inij) / 100 ) ),'-');
+        plot(inij+1: displayN : j ,res1m(1:ceil( (j - inij) / displayN ) ),'x');
         drawnow;
-        save([resPath 'currentphase.mat'],'curPhi','j');
-        %localcurphi = gather(curphi);
+        save([resPath 'currentphase.mat'],'curPhi','j','coef');
     end
 end
 
