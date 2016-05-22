063c2c0d826873d3c42fc451fc7229dc95cfa303
kenkangxgwe <kenkangxgwe@gmail.com>
2016-05-22 13:09:35 +0800

    Data from 21 May 2016

--- phaseRetrivalCaller.m
+++ phaseRetrivalCaller.m
@@ -1,41 +1,42 @@
 %% phaseRetrivalCaller
-% A function caller, which no only calls functions step by step, but can also
+% A function caller, which not only calls functions step by step, but can also
 % jump to a specific function directly if the program has finished the former
 % functions before.
 
 wavelength = 0.7667;
 pixelsize = 6.5;
-step = 6;
-path = 'G:\新建文件夹\Experiment\7Apr2016\';
+step = bin2dec('001');
+path = 'G:\新建文件夹\Experiment\21May2016\';
 
-refZ = - 0.0505 * 25400; % Distance in mm
-z1 = - 0.2721 * 25400; 
-z2 = - 0.3500 * 25400;
-z3 = - 0.5000 * 25400;
-focZ = - 0.6080 * 25400;
+refZ = 19600 * 0.625; % A
+z1 = 6200 * 0.625; % B5
+z2 = -300 * 0.625; % B3
+z3 = - 5300 * 0.625; %B1
+focZ = - 7800 * 0.625; % E
 
 %% RefPhaseRetrival1
 % A iterative algorithm to get the phase infomation of image a1
 
 if(bitand(step, 1) )
-    %From top to bottom, the distance decreases and the light spot shrinks
-    a1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveB', 'up', 1, 100);
-    bg1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveBbk', 'up', 1, 100);
+    
+% Loading the image in the order of the size from big to smail
+    a1 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveB5_p6200a', 'up', 1, 100);
+    bg1 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveB5bk_p6200a', 'up', 1, 100);
     a1 = max(a1 - bg1, 0);
 
-    a2 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveC', 'up', 1, 100);
-    bg2 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveCbk', 'up', 1, 100);
+    a2 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveB3_n300a', 'up', 1, 100);
+    bg2 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveB3bk_n300a', 'up', 1, 100);
     a2 = max(a2 - bg2, 0);
     
-    a3 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveD', 'up', 1, 100);
-    bg3 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveDbk', 'up', 1, 100);
+    a3 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveB1_n5300a', 'up', 1, 100);
+    bg3 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveB1bk_n5300a', 'up', 1, 100);
     a3 = max(a3 - bg3, 0);
-    
-    a4 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveE', 'up', 1, 100);
-    bg4 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveEbk', 'up', 1, 100);
+
+    a4 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveE_n7800a', 'up', 1, 100);
+    bg4 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveEbk_n7800a', 'up', 1, 100);
     a4 = max(a4 - bg4, 0);
 
-    RefPhaseRetrival1(a4, a3, a2, a1, z4, z3, z2, z1, pixelsize, wavelength, path);
+    RefPhaseRetrival1(a4, a3, a2, a1, focZ, z3, z2, z1, pixelsize, wavelength, path);
 end
 
 %% RefPhaseRetrival2
@@ -43,11 +44,11 @@ end
 % image via image a1
 
 if(bitand(step, 2) )
-    refimg = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveA', 'up', 1, 100);
-    refbg = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveAbk', 'up', 1, 100);
+    refimg = openxlsFigures([path 'WavefrontMeasurement\'], 'waveA_p19600a', 'up', 1, 100);
+    refbg = openxlsFigures([path 'WavefrontMeasurement\'], 'waveAbk_p19600a', 'up', 1, 100);
     refimg = max(refimg - refbg, 0);
-    a1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveB', 'up', 1, 100);
-    bg1 = openxlsFigures(strcat(path,'WavefrontMeasurement\'), 'waveBbk', 'up', 1, 100);
+    a1 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveB5_p6200a', 'up', 1, 100);
+    bg1 = openxlsFigures([path 'WavefrontMeasurement\'], 'waveB5_p6200a', 'up', 1, 100);
     a1 = max(a1 - bg1, 0);
 
     [refPhi] = RefPhaseRetrival2(a1, z1, refimg, refZ, pixelsize, wavelength, path);
@@ -58,10 +59,12 @@ end
 % Get the signal holograph via reference intensity, phase and signal intensity
 
 if(bitand(step, 4) )
-    atomCut = openxlsFigures(strcat(path,'OpticalLattice_MOT1d0V_Exp1d0ms\'), 'OpticalLattice_MOT1d0V_Exp1d0ms200', 'up');
-    atomCutRef = openxlsFigures(strcat(path,'OpticalLattice_MOT1d0V_Exp1d0ms\'), 'OpticalLattice_MOT1d0V_Exp1d0ms200', 'down');
-    atomCutBk1 = openxlsFigures(strcat(path,'OpticalLattice_MOT1d0V_Exp1d0ms\'), 'BK', 'up', 2, 10);
-    atomCutBk2 = openxlsFigures(strcat(path,'OpticalLattice_MOT1d0V_Exp1d0ms\'), 'BK', 'down', 2, 10);
+
+     atomCut = openxlsFigures(strcat(path,'OpticalLattice_MOT1d0V_Exp0d2ms\'), 'OpticalLattice_MOT1d0V_Exp0d2ms10', 'up');
+     atomCutRef = openxlsFigures(strcat(path,'OpticalLattice_MOT1d0V_Exp0d2ms\'), 'OpticalLattice_MOT1d0V_Exp0d2ms10', 'down');
+     atomCutBk1 = openxlsFigures(strcat(path,'OpticalLattice_MOT1d0V_Exp0d2ms\'), 'BK', 'up', 1, 21);
+     atomCutBk2 = openxlsFigures(strcat(path,'OpticalLattice_MOT1d0V_Exp0d2ms\'), 'BK', 'down', 1, 21);
+
     [coRef, coHolbg, coRefbg] = HologramOptimization1(atomCut, atomCutRef, refZ, atomCutBk1, atomCutBk2, pixelsize, wavelength, path);
 %     coRef = 1; coHolbg = 1; coRefbg = 0;
     SignalImageRetrival1(atomCut, coRef * atomCutRef, refZ, focZ, coHolbg * atomCutBk1, coRefbg * atomCutBk2, pixelsize, wavelength, path);
