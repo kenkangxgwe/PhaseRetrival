8e15dc8a3a638ee23150dd94786266d1232fc925
kenkangxgwe <kenkangxgwe@gmail.com>
2016-05-28 23:24:20 +0800

    new way to calculate residue

--- RefPhaseRetrival2.m
+++ RefPhaseRetrival2.m
@@ -1,4 +1,4 @@
-function tarPhi = RefPhaseRetrival2(srcimg, srcZ, tarimg, tarZ, pixelsize, wavelength, FilePath)
+function tarPhi = RefPhaseRetrival2(srcImg, srcZ, tarImg, tarZ, pixelsize, wavelength, FilePath)
 
 gpuDevice(1);
 resize = 6;
@@ -7,15 +7,14 @@ NAs = 0.35;
 resPath = [FilePath 'Result\'];
 
 if(exist([resPath 'currentphase.mat'], 'file') )
-    load([resPath 'currentphase.mat'], 'curPhiSmoothed');
-    srcPhi = curPhi;
+    load([resPath 'currentphase.mat'], 'curPhi');
+    srcPhi = curPhiSmoothed;
 else
     error('currentphase.mat does not exist');
 end
 
-srcimg = double(imresize(srcimg, resize, 'nearest') );
-[rowSize, colSize] = size(srcimg);
-gpuSrcimg = gpuArray(srcimg);
+gpuSrcImg = sqrt(complex(imresize(gpuArray(srcImg), resize) ) );
+[rowSize, colSize] = size(gpuSrcImg);
 mesh = pixelsize / resize;
 
 % extend the image on x direction
@@ -25,14 +24,12 @@ kmeshx = 2 * pi / (mesh * exSize);
 kmeshy = 2 * pi / (mesh * eySize);
 kx = ( (1 : exSize) - exSize / 2 - 1) * kmeshx; 
 ky = ( (1 : eySize) - eySize / 2 - 1) * kmeshy;
-kx = fftshift(kx);
-ky = fftshift(ky);
+kx = fftshift(kx); ky = fftshift(ky);
 [kkx, kky] = meshgrid(kx, ky);
-kkx = gpuArray(kkx);
-kky = gpuArray(kky);
+kkx = gpuArray(kkx); kky = gpuArray(kky);
 kwindow = exp(- (kkx .^ 2+kky .^ 2) / k^2 / NAs^2);
 kwindow = kwindow > (max(kwindow(:) ) / 2.71828);
-tarWavex = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (tarZ - srcZ) ) .* fft2(gpuSrcimg .* exp(1i * srcPhi), eySize, exSize) );
+tarWavex = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (tarZ - srcZ) ) .* fft2(gpuSrcImg .* exp(1i * srcPhi), eySize, exSize) );
 tarWavex = tarWavex(1: rowSize , 1 : colSize);
 clear kkx kky;
 
@@ -44,21 +41,26 @@ kmeshx = 2 * pi / (mesh * exSize);
 kmeshy = 2 * pi / (mesh * eySize);
 kx = ( (1 : exSize) - exSize / 2 - 1) * kmeshx; 
 ky = ( (1 : eySize) - eySize / 2 - 1) * kmeshy;
-kx = fftshift(kx);
-ky = fftshift(ky);
+kx = fftshift(kx); ky = fftshift(ky);
 [kkx, kky] = meshgrid(kx, ky);
-kkx = gpuArray(kkx);
-kky = gpuArray(kky);
+kkx = gpuArray(kkx); kky = gpuArray(kky);
 kwindow = exp(- (kkx .^ 2+kky .^ 2) / k^2 / NAs^2);
 kwindow = kwindow > (max(kwindow(:) ) / 2.71828);
-tarWavey = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (tarZ - srcZ) ) .* fft2(gpuSrcimg .* exp(1i * srcPhi), eySize, exSize) );
+tarWavey = ifft2(kwindow .* exp(1i * sqrt(k^2 - kkx .^ 2 - kky .^ 2) * (tarZ - srcZ) ) .* fft2(gpuSrcImg .* exp(1i * srcPhi), eySize, exSize) );
 tarWavey = tarWavey(1: rowSize , 1 : colSize);
 clear kkx kky;
 
 % Combine two different Images
 tarWave = [tarWavey(1: rowSize, 1 : 7200), tarWavex(1: rowSize, 7201 : colSize)];
 tarPhi = angle(tarWave);
-tarWave = gather(abs(tarWave) );
-tarWave = double(imresize(tarWave, 1 / resize, 'nearest') );
-figure; imagesc(abs(tarWave) ); drawnow;
-residue = mean(abs( abs( tarWave(tarimg ~= 0) ) / mean( abs( tarWave(tarimg ~= 0) ) ) - abs(tarimg(tarimg ~= 0) / mean(tarimg(tarimg ~= 0) ) ) ) ./ (abs(tarimg(tarimg ~= 0) ) / mean(tarimg(tarimg ~= 0) ) ) )
+tarWave = double(gather(imresize(abs(tarWave).^2, 1 / resize) ) );
+figure; 
+subplot(1,2,1); imagesc(abs(tarWave) ); title('propagated image'); 
+subplot(1,2,2); imagesc(abs(tarImg) ); title('original image');
+drawnow; 
+
+% Calculate the residue
+a = tarWave(100 : 1000, 200 : 1100); a = a(:);
+b = tarImg(100 : 1000, 200 : 1100); b = b(:);
+
+residue = 1 - (dot(a - mean(a), b - mean(b) ) / (size([a; b], 2) * std(a) * std(b) );
